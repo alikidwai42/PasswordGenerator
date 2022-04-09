977aa6c215e72b7f985dcf432401045fd75a2261
fauzank339 <50906187+fauzank339@users.noreply.github.com>
2022-04-09 14:50:55 +0530

    Update src.py

--- src.py
+++ src.py
@@ -1,1 +1,13 @@
+import random
 
+CharacterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_=?.,:\|}{[]"
+while 1:
+    password_length = int(input("Length of the password required: "))
+    password_count = int(input("How many passwords you need: "))
+    
+    for count in range(password_count):
+        password=""
+        for x in range(password_length):
+            password_character=random.choice(CharacterSet)
+            password = password+password_character
+        print("Here is your password: ",password)
